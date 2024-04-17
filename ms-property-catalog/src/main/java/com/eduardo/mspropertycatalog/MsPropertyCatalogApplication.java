48836ce5a84ee5ca69e64a7b19b41ccf9c42aeb6
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-04-17 20:17:25 -0300

    Add configs iniciais do ms-user e do ms-property-catalog

---
+++ ms-property-catalog/src/main/java/com/eduardo/mspropertycatalog/MsPropertyCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.mspropertycatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPropertyCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPropertyCatalogApplication.class, args);
+	}
+
+}
