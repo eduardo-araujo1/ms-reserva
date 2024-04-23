d5bb0e075b2384c00da5d4a2c6041250d5f58f1d
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-04-23 20:58:07 -0300

    Adicionando o ms-reservation

---
+++ ms-reservation/src/main/java/com/eduardo/msreservation/MsReservationApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.msreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsReservationApplication.class, args);
+	}
+
+}
