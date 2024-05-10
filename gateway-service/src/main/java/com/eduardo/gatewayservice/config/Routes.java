4e58665479031a815c5dc6fd37a734db46116fe2
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-05-10 19:05:24 -0300

    Pequena refatoracao na classe Routes do gateway-service e adicionando o gateway no docker-compose

--- gateway-service/src/main/java/com/eduardo/gatewayservice/config/Routes.java
+++ gateway-service/src/main/java/com/eduardo/gatewayservice/config/Routes.java
@@ -1,5 +1,6 @@
 package com.eduardo.gatewayservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +9,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Routes {
 
+    @Value("${user-service.url}")
+    private String userServiceUrl;
+
+    @Value("${property-service.url}")
+    private String propertyServiceUrl;
+
+    @Value("${reservation-service.url}")
+    private String reservationServiceUrl;
+
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("ms-user", r -> r.path("/v1/users/**")
-                        .uri("http://localhost:8081"))
+                        .uri(userServiceUrl))
                 .route("ms-property-catalog", r -> r.path("/v1/properties/**")
-                        .uri("http://localhost:8082"))
+                        .uri(propertyServiceUrl))
                 .route("ms-reservation", r -> r.path("/v1/reservations/**")
-                        .uri("http://localhost:8083"))
+                        .uri(reservationServiceUrl))
                 .build();
     }
-
 }
